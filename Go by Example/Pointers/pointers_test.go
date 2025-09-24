3597b15d891dcc17b079f2d3d0294c8afd582357
Danny Kan <not.danny.kn@gmail.com>
2025-09-24 10:57:09 -0400

    dev: added a pointers.go file and a pointers_test.go file.

---
+++ Go by Example/Pointers/pointers_test.go
@@ -0,0 +1,50 @@
+package main
+
+import "testing"
+
+func Test_main(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			main()
+		})
+	}
+}
+
+func Test_zeroval(t *testing.T) {
+	type args struct {
+		ival int
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			zeroval(tt.args.ival)
+		})
+	}
+}
+
+func Test_zeroptr(t *testing.T) {
+	type args struct {
+		iptr *int
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			zeroptr(tt.args.iptr)
+		})
+	}
+}
