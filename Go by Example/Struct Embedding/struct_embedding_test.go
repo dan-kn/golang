a3effc5d665298dd9687a226f996d643fa4e4484
Danny Kan <not.danny.kn@gmail.com>
2025-09-24 15:58:22 -0400

    dev: added a struct_embedding.go file and a struct_embedding_test.go file.

---
+++ Go by Example/Struct Embedding/struct_embedding_test.go
@@ -0,0 +1,39 @@
+package main
+
+import "testing"
+
+func Test_base_describe(t *testing.T) {
+	type fields struct {
+		num int
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			b := base{
+				num: tt.fields.num,
+			}
+			if got := b.describe(); got != tt.want {
+				t.Errorf("base.describe() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_main(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			main()
+		})
+	}
+}
